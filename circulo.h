461eb57624e86122e80a08eb01fa54dea3a07d21
noemartires <noemartires15@gmail.com>
2022-01-09 20:24:33 +0000

    alteraçoes distancia circulo.h

--- circulo.h
+++ circulo.h
@@ -1,1 +1,1 @@
-float  DistanciaDeAaB(PONTO A, PONTO B){/*...*/}float  AreaCirculo(float  raio){/*...*/}float  PerimetroCircunferencia(float  raio){/*...*/}void  InfoCirculo (){/*...*/}
+float  DistanciaDeAaB(PONTO A, PONTO B){/* por  implementar */}float  AreaCirculo(float  raio){return  M_PI * raio * raio;}float  PerimetroCircunferencia(float  raio){return 2 * M_PI * raio;}void  InfoCirculo (){/*...*/}
